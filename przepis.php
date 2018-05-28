23e915194b08b9d0dfb889359a18761a32af408c
Solinski Piotr <solinski.mfs@gmail.com>
2018-05-28 23:34:49 +0200

    Druga wersja aplikacji

--- przepis.php
+++ przepis.php
@@ -12,7 +12,8 @@
 		$polaczenie->query("SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
 		$polaczenie->query("SET CHARSET utf8");
 		$rezultat = $polaczenie->query("SELECT p.*,sp.nazwa_spos_przygotowania FROM przepisy AS p LEFT JOIN sposob_przygotowania AS sp ON p.id_spos_przygotowania=sp.id_spos_przygotowania WHERE p.id_przepisu='$id_przepisu'");
-
+		$rezultat_komentarze = $polaczenie->query("SELECT uzytkownicy.uzytkownik, komentarze.komentarz, komentarze.data FROM komentarze LEFT JOIN przepisy ON przepisy.id_przepisu=komentarze.id_przepisu LEFT JOIN uzytkownicy ON komentarze.id_uzytkownika=uzytkownicy.id_uzytkownika WHERE komentarze.id_przepisu='1' ORDER BY komentarze.data DESC");
+		
 		$wiersz = $rezultat->fetch_assoc();
 		$tytul_slowa = explode(" ", $wiersz['tytul']); //pierwsze słowo tytułu
 
@@ -22,6 +23,20 @@
 	} catch (Exception $e) {
 		echo '<span style="color:red;">Błąd serwera przepraszamy na niedogodności</span>';
 		echo "Informacja developera: ".$e;
+	}
+
+	function informacja() {
+		global $wiersz;
+		if($wiersz['d_bezglutenowe'] == 1)
+			echo '<i class="icon-check"></i>Danie bezglutenowe<br>';
+		if($wiersz['d_wegetarianskie'] == 1)
+			echo '<i class="icon-check"></i>Danie wegetariańskie<br>';
+		if($wiersz['d_dietetyczne'] == 1)
+			echo '<i class="icon-check"></i>Danie dietetyczne<br>';
+		if($wiersz['d_dla_dzieci'] == 1)
+			echo '<i class="icon-check"></i>Danie dla dzieci<br>';
+		if($wiersz['szybka_kuchnia'] == 1)
+			echo '<i class="icon-check"></i>Szybka kuchnia<br>';
 	}
 ?>
 <!DOCTYPE html>
@@ -52,35 +67,66 @@
 			<img src="obrazy_potraw/<?php echo $id_przepisu; ?>.jpg">
 		</div>
 		<div class="przepis_info_przepisu">
-			<div class="pipt">
-				<?php echo $wiersz['nazwa_spos_przygotowania'] ?>
-			</div>
 			<dl>
+				<dt>Sposób przygotowania:</dt>
+				<dd><i class="icon-lightbulb"></i><?php echo $wiersz['nazwa_spos_przygotowania']; ?></dd>
 				<dt>Czas przygotowania:</dt>
-				<dd><?php echo $wiersz['czas_przygotowania']; ?> min.</dd>
+				<dd><i class="icon-clock-black"></i><?php echo $wiersz['czas_przygotowania']; ?> min.</dd>
 				<dt>Liczba porcji:</dt>
-				<dd><?php echo $wiersz['liczba_porcji']; ?> porcje</dd>
+				<dd><i class="icon-food"></i><?php echo $wiersz['liczba_porcji']; ?> porcje</dd>
+				<dt>Inne informację:</dt>
+				<dd><?php informacja(); ?></dd>
 				<dt>Ocena przepisu:</dt>
+				<dd>
+					<i class="icon-star-empty" name="1"></i>
+					<i class="icon-star-empty" name="2"></i>
+					<i class="icon-star-empty" name="3"></i>
+					<i class="icon-star-empty" name="4"></i>
+					<i class="icon-star-empty" name="5"></i>
+				</dd>
 			</dl>
 		</div>
 		<div style="clear: both;"></div>
-		<div class="skladniki">
-			<h3>Skladniki:</h3>
+		<div class="przepis_skladniki">
+			<div class="pipt">Skladniki:</div>
 			<ul>
-			<?php 
-				$skladniki = explode(",", $wiersz['skladniki']);
+				<?php $skladniki = explode(",", $wiersz['skladniki']);
 				count($skladniki);
 				for ($i=0; $i < 6; $i++) { 
-					echo '<li>'.$skladniki[$i].'</li><hr>';
-				}
-			?>
+					echo '<li>'.$skladniki[$i].'</li>';
+				} ?>
 			</ul>
 		</div>
-		<div class="przygotowanie">
-			<h3>Przygotowanie:</h3>
+		<div class="przepis_przygotowanie">
+			<div class="pipt">Przygotowanie:</div>
 			<?php 
 				echo nl2br($wiersz['przygotowanie']); 
 			?> 
+		</div>
+		<div style="clear: both;"></div>
+		<hr>
+		<div class="przepis_k">
+			<div class="pipt">Komentarze:</div>
+			<div class="przepis_dk">
+				<input type="text" name="przepis_dk" placeholder="Dodaj publiczny komentarz">
+				<button class="przepis_dk_p">Skomentuj</button>
+			</div>
+			<div class="przepis_wk">
+				<?php while ($wiersz_komentarz = $rezultat_komentarze->fetch_assoc()) { ?>
+					<div class="przepis_pk">
+						<div class="przepis_pk_u">
+							<i class="icon-user-circle-o"></i><?php echo $wiersz_komentarz['uzytkownik']; ?>
+						</div>
+						<div class="przepis_pk_d">
+							<?php echo $wiersz_komentarz['data']; ?>
+						</div>
+						<div style="clear: both;"></div>
+						<div class="przepis_pk_k">
+							<?php echo $wiersz_komentarz['komentarz']; ?>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
 </div>
 </div>
